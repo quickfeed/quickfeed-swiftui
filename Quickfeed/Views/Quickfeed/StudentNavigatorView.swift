38d7124406f378f2f2d72b9e93d3d00f6e396c1b
Oskar Gjølga <os.gjolga@stud.uis.no>
2021-01-25 20:33:16 +0100

    course picker and lab menu

--- Quickfeed/Views/Quickfeed/StudentNavigatorView.swift
+++ Quickfeed/Views/Quickfeed/StudentNavigatorView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct StudentNavigatorView: View {
+    var student: UserModel
+    @State private var selectedCourse = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+           
+            Picker(selection: $selectedCourse, label: Text("Current course")) {
+                ForEach(0 ..< student.enrolledIn.count){
+                    Text(student.enrolledIn[$0].courseCode)
+                        
+                        
+                    
+                }
+                
+            }
+            .pickerStyle(MenuPickerStyle())
+            .labelsHidden()
+            .padding(.bottom, 5)
+            LabMenuView(labs: student.enrolledIn[selectedCourse].labs)
+                
+        }
     }
 }
 
 struct StudentNavigatorView_Previews: PreviewProvider {
+    static var student = UserModel.data[0]
     static var previews: some View {
-        StudentNavigatorView()
+        StudentNavigatorView(student: student)
     }
 }
