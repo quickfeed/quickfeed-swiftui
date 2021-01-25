ad106615817cb0b1776dd4b2c18ad3232f1092a2
Oskar Gjølga <os.gjolga@stud.uis.no>
2021-01-25 09:12:29 +0100

    Initial Commit

---
+++ Quickfeed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quickfeed
+//
+//  Created by Oskar Gjølga on 25/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quickfeed/QuickfeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickfeedApp.swift
+//  Quickfeed
+//
+//  Created by Oskar Gjølga on 25/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct QuickfeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
