8004e35f013b47ed18e5d16cbb71c84df177a7aa
Oskar Gjølga <os.gjolga@stud.uis.no>
2021-02-10 20:28:14 +0100

    review list item

---
+++ Quickfeed/Views/ListItems/SubmissionListItem.swift
@@ -0,0 +1,20 @@
+//
+//  SubmissionListItem.swift
+//  Quickfeed
+//
+//  Created by Oskar Gjølga on 10/02/2021.
+//
+
+import SwiftUI
+
+struct SubmissionListItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SubmissionListItem_Previews: PreviewProvider {
+    static var previews: some View {
+        SubmissionListItem()
+    }
+}
---
+++ Quickfeed/Views/Teacher/ReviewNavigatorView.swift
@@ -0,0 +1,55 @@
+//
+//  ReviewNavigatorView.swift
+//  Quickfeed
+//
+//  Created by Oskar Gjølga on 07/02/2021.
+//
+
+import SwiftUI
+
+struct ReviewNavigatorView: View {
+    @State private var searchQuery: String = ""
+    @State var users: [User]
+    @Binding var selectedLab: UInt64
+    @EnvironmentObject var viewModel: TeacherViewModel
+    
+    
+    
+    
+    func matchesQuery(str: String) -> Bool{
+        if searchQuery != "" && !str.lowercased().contains(self.searchQuery.lowercased()){
+            return false
+        }
+        return true
+    }
+    
+    var body: some View {
+        NavigationView{
+            VStack(alignment: .leading){
+                Text("Review Submissions")
+                    .font(.headline)
+                LabPicker(labs: viewModel.courses[0].assignments, selectedLab: $selectedLab)
+                    .frame(width: 200)
+                SearchFieldRepresentable(query: $searchQuery)
+                
+                List{
+                    Section(header: Text("Students")){
+                        ForEach(users.filter({ matchesQuery(str: $0.name) }), id: \.id){ user in
+                            NavigationLink(destination: Text(user.name)){
+                                Text(user.name)
+                            }
+                        }
+                    }
+                }
+            }
+            .padding(5)
+        }
+    }
+}
+
+struct ReviewNavigatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReviewNavigatorView(users: [], selectedLab: .constant(1))
+            .environmentObject(TeacherViewModel(provider: FakeProvider()))
+    }
+}
