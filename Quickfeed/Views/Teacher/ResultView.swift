2b5b6f3c37fa1984612c1569f3b4b858c6d27351
Oskar Gjølga <os.gjolga@stud.uis.no>
2021-02-23 12:22:15 +0100

    results view

---
+++ Quickfeed/Views/Teacher/ResultView.swift
@@ -0,0 +1,41 @@
+//
+//  ResultView.swift
+//  Quickfeed
+//
+//  Created by Oskar Gjølga on 23/02/2021.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    @EnvironmentObject var viewModel: TeacherViewModel
+    @Binding var selectedCourse: UInt64
+    @State var users: [User] = []
+    @State var searchQuery: String = ""
+    @State private var displayingSubmission = false
+    
+    var body: some View {
+        
+        if !displayingSubmission {
+            ResultGrid(displayingSubmission: $displayingSubmission, selectedCourse: $selectedCourse)
+        }
+        if displayingSubmission {
+            // SubmissionResults(show: $show)
+                
+        }
+    
+        
+        
+        
+    }
+    
+    
+    
+}
+
+struct ResultGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultView(selectedCourse: .constant(111))
+            .environmentObject(TeacherViewModel(provider: FakeProvider()))
+    }
+}
