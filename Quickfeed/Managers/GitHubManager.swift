4f2a3c1d6d390c03006f7f920aa47713b21acdef
Bjørn Kristian Teisrud <b.k.teisrud@gmail.com>
2021-02-23 09:11:51 +0100

    GitHub Button & GitHub Manager

---
+++ Quickfeed/Managers/GitHubManager.swift
@@ -0,0 +1,16 @@
+//
+//  GitHubManager.swift
+//  Quickfeed
+//
+//  Created by Bjørn Kristian Teisrud on 23/02/2021.
+//
+
+import Foundation
+
+struct GitHubConstants {
+    static let CLIENT_ID = "7dc199d467863a0e88b1"
+    static let CLIENT_SECRET = "16f9cb941aa55ffea6209ba106831edb70b20acc"
+    static let REDIRECT_URI = "https://localhost:8080/"
+    static let SCOPE = "read:user"
+    static let TOKENURL = "https://github.com/login/oauth/access_token"
+}
