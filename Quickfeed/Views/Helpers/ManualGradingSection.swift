dd1108c73bb4628691525933b8e2e3bb37d52dfa
Oskar Gjølga <os.gjolga@stud.uis.no>
2021-02-24 11:46:07 +0100

    removed

---
+++ Quickfeed/Views/Helpers/ManualGradingSection.swift
@@ -0,0 +1,20 @@
+//
+//  ManualGradingSection.swift
+//  Quickfeed
+//
+//  Created by Oskar Gjølga on 24/02/2021.
+//
+
+import SwiftUI
+
+struct ManualGradingSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ManualGradingSection_Previews: PreviewProvider {
+    static var previews: some View {
+        ManualGradingSection()
+    }
+}
