ab781ac8be531807d96134bd83e0cad57667b313
Oskar Gjølga <os.gjolga@stud.uis.no>
2021-03-12 08:13:56 +0100

    review filter toggles

--- Quickfeed/Views/Teacher/Review/ReviewNavigationView.swift
+++ Quickfeed/Views/Teacher/Review/ReviewNavigationView.swift
@@ -11,8 +11,37 @@ struct ReviewNavigationView: View {
     @ObservedObject var viewModel: TeacherViewModel
     @State private var searchQuery: String = ""
     @Binding var selectedLab: UInt64
-    @State private var showCompleted: Bool = true
-    @State private var showMissing: Bool = false
+    @State private var filterReady: Bool = false
+    @State private var filterInProgress: Bool = false
+    @State private var filterNone: Bool = false
+    @State private var filterApproved: Bool = false
+    @State private var filterRevision: Bool = false
+    @State private var filterNoReviews: Bool = false
+    
+    
+    var filteredEnrollmentLinks: [EnrollmentLink] {
+        return viewModel.enrollmentLinks.filter({
+                matchesQuery(user: $0.enrollment.user)
+                    && isShown(link: submissionForSelectedLab(links: $0.submissions))
+        })
+    }
+    func isShown(link: SubmissionLink) -> Bool{
+        if filterReady{
+            for review in link.submission.reviews{
+                if review.ready{
+                    continue
+                }
+            }
+            return false
+        }
+        if filterNoReviews{
+            if link.submission.reviews.count > 0{
+                return false
+            }
+        }
+        
+        return true
+    }
     
     
     func submissionForSelectedLab(links: [SubmissionLink]) -> SubmissionLink {
@@ -22,23 +51,19 @@ struct ReviewNavigationView: View {
     }
     
     
-    func isShown(link: SubmissionLink) -> Bool{
-        if !showCompleted{
-            for review in link.submission.reviews{
-                if review.ready{
-                    return false
-                }
-               
+    
+    
+    func countMissingSubmissions() -> Int{
+        var count = 0
+        for enrollmentLink in viewModel.enrollmentLinks{
+            if !enrollmentLink.submissions.contains(where: {$0.submission.assignmentID == selectedLab}){
+                count += 1
             }
         }
-        if !showMissing{
-            if !link.hasSubmission{
-                return false
-            }
-        }
-        
-        return true
+        return count
     }
+    
+  
     
     func matchesQuery(user: User) -> Bool{
         if searchQuery == ""{
@@ -69,12 +94,24 @@ struct ReviewNavigationView: View {
                 SearchFieldRepresentable(query: $searchQuery)
                     .frame(height: 25)
                 
-                Toggle("Show completed", isOn: $showCompleted)
-                Toggle("Show missing", isOn: $showMissing)
                 
+                HStack{
+                    Toggle("Ready", isOn: $filterReady)
+                    Toggle("In progress", isOn: $filterInProgress)
+                    Toggle("No reviews", isOn: $filterNoReviews)
+                }
+                HStack{
+                    Toggle("None", isOn: $filterNone)
+                    Toggle("Approved", isOn: $filterApproved)
+                    Toggle("Revision", isOn: $filterRevision)
+                }
+                
+                    
+                    
+              
                 List{
                     Section(header: SubmissionListHeader()){
-                        ForEach(viewModel.enrollmentLinks.filter({ matchesQuery(user: $0.enrollment.user) && isShown(link: submissionForSelectedLab(links: $0.submissions))}), id: \.enrollment.user.id){ link in
+                        ForEach(filteredEnrollmentLinks, id: \.enrollment.user.id){ link in
                             NavigationLink(destination: SubmissionReview(user: link.enrollment.user, viewModel: viewModel, submissionLink: submissionForSelectedLab(links: link.submissions))){
                                 VStack{
                                     SubmissionListItem(submitterName: link.enrollment.user.name, subLink: submissionForSelectedLab(links: link.submissions))
