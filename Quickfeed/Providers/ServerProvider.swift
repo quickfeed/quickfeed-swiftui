fe4d02d425bc1aef8bac4f40e027c7f59a29f28d
Oskar Gjølga <os.gjolga@stud.uis.no>
2021-03-18 11:45:22 +0100

    Serverprovider cleanup

--- Quickfeed/Providers/ServerProvider.swift
+++ Quickfeed/Providers/ServerProvider.swift
@@ -9,9 +9,6 @@ import NIO
 
 class ServerProvider: ProviderProtocol{
     
-    func getUsersForCourse(course: Course) -> [User] {
-        fatalError("Not implemented")
-    }
     
     var currentUser: User
     var grpcManager: GRPCManager
@@ -80,6 +77,11 @@ class ServerProvider: ProviderProtocol{
     func addUserToCourse(course: Course, user: User) -> Bool {
         fatalError("Not implemented")
     }
+    
+    func getUsersForCourse(course: Course) -> [User] {
+        fatalError("Not implemented")
+    }
+    
     
     func changeUserStatus(enrollment: Enrollment, status: Enrollment.UserStatus) -> Status {
         fatalError("Not implemented")
