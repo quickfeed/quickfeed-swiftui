f3bb940ad05b2ebae4ae86eb840113f81a6a4039
Bjørn Kristian Teisrud <b.k.teisrud@gmail.com>
2021-03-18 20:19:54 +0100

    teacherviewmodel

--- Quickfeed/ViewModels/Users/TeacherViewModel.swift
+++ Quickfeed/ViewModels/Users/TeacherViewModel.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Oskar Gjølga on 03/02/2021.
 //
-
 import Foundation
 import Combine
 import NIO
@@ -15,7 +14,7 @@ class TeacherViewModel: UserViewModelProtocol{
     @Published var currentCourse: Course
     @Published var enrollments: [Enrollment] = []
     @Published var users: [User] = []
-    @Published var courses: [Course] = []
+    
     @Published var groups: [Group] = []
     @Published var assignments: [Assignment] = []
     @Published var manuallyGradedAssignments: [Assignment] = []
@@ -37,14 +36,6 @@ class TeacherViewModel: UserViewModelProtocol{
     }
     
     
-    func getCourse(courseId: UInt64) -> Course{
-        for course in self.courses{
-            if course.id == courseId{
-                return course
-            }
-        }
-        return Course()
-    }
     
     func loadUsers(){
         for enrollment in self.enrollments{
@@ -52,10 +43,7 @@ class TeacherViewModel: UserViewModelProtocol{
         }
     }
     
-    
-    func loadCourses() {
-        self.courses = self.provider.getCourses()!
-    }
+
     
     func loadGroups(){
         let response = self.provider.getGroupsByCourse(courseId: self.currentCourse.id)
@@ -127,16 +115,7 @@ class TeacherViewModel: UserViewModelProtocol{
     }
     
     
-    func getAssignmentById(id: UInt64) -> Assignment{
-        for course in self.courses{
-            for assignment in course.assignments{
-                if assignment.id == id{
-                    return assignment
-                }
-            }
-        }
-        return Assignment()
-    }
+  
     
     func getUserName(userId: UInt64) -> String{
         for user in self.users{
