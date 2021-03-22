fddf20158b94f56fb2edade9fa72edf3e5abb94f
Bjørn Kristian Teisrud <b.k.teisrud@gmail.com>
2021-03-22 10:55:35 +0100

    small start on new course page

--- Quickfeed/Views/Admin/Courses/NewOrEditCourse.swift
+++ Quickfeed/Views/Admin/Courses/NewOrEditCourse.swift
@@ -10,20 +10,71 @@ import SwiftUI
 struct NewOrEditCourse: View {
     var course: Course?
     @Binding var editCourse: Bool
+    @State var organization: String = ""
     
     var body: some View {
-        Text(course != nil ? "Edit \(course!.code)" : "New Course")
-            .navigationTitle(course != nil ? "\(course!.code) \(course!.name)" : "New Course")
-            .toolbar{
-                ToolbarItem(placement: .navigation){
-                    Toggle(isOn: $editCourse, label: {
-                        Image(systemName: "chevron.backward")
+        if course == nil{
+            VStack(alignment: .leading){
+                Text("""
+                    For each new semester of a course, QuickFeed requires a new GitHub organization.
+                    This is to keep the student roster for the different runs of the course separate.
+                    """)
+                HStack{
+                    Link(destination: URL(string: "https://github.com/account/organizations/new")!, label:{
+                        Text("Create an organization for your course.")
                     })
-                    .keyboardShortcut("b")
-                    .help("Return to course list")
+                    Text("The course organization must allow private repositories.")
                 }
+                Text("QuickFeed will create a following repository structure for you:")
+                Text("""
+                    - course-info
+                    - assignments
+                    - tests
+                    """)
+                    .padding(.leading)
+                HStack{
+                    Text("Please read")
+                    Link(destination: URL(string: "https://github.com/autograde/quickfeed/blob/master/doc/teacher.md")!, label:{
+                        Text("the documentation")
+                    })
+                    Text("for further instructions on how to work with the various repositories.")
+                }
+                Form{
+                    TextField("Enter Organization Name...", text: $organization)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .frame(width: 450)
+                    Text("Your Organization: \(organization)")
+                }
+                
             }
-            .onDisappear(perform: { self.editCourse = true })
+            .padding()
+            Spacer()
+                .navigationTitle("New Course")
+                .toolbar{
+                    ToolbarItem(placement: .navigation){
+                        
+                        Toggle(isOn: $editCourse, label: {
+                            Image(systemName: "chevron.backward")
+                        })
+                        .keyboardShortcut("b")
+                        .help("Return to course list")
+                    }
+                }
+                .onDisappear(perform: { self.editCourse = true })
+        } else {
+            Text("Edit \(course!.code)")
+                .navigationTitle("\(course!.code): \(course!.name)")
+                .toolbar{
+                    ToolbarItem(placement: .navigation){
+                        Toggle(isOn: $editCourse, label: {
+                            Image(systemName: "chevron.backward")
+                        })
+                        .keyboardShortcut("b")
+                        .help("Return to course list")
+                    }
+                }
+                .onDisappear(perform: { self.editCourse = true })
+        }
     }
 }
 
