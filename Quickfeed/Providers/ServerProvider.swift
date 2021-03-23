e9bcfb62f24b5ba7eefd698d3fb4dd7cb8a49ed5
Bjørn Kristian Teisrud <b.k.teisrud@gmail.com>
2021-03-23 22:44:51 +0100

    change user

--- Quickfeed/Providers/ServerProvider.swift
+++ Quickfeed/Providers/ServerProvider.swift
@@ -14,7 +14,7 @@ class ServerProvider: ProviderProtocol{
     var grpcManager: GRPCManager
   
     init() {
-        let userID = UInt64(116)
+        let userID = UInt64(151)
         self.grpcManager = GRPCManager(userID: userID)
         self.currentUser = self.grpcManager.getUser(userId: userID) ?? User()
     }
