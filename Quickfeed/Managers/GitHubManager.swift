2c2c704f3392fbcdaf6a87d0e5bda21ebf817755
Bjørn Kristian Teisrud <b.k.teisrud@gmail.com>
2021-04-20 11:33:53 +0200

    removed useless commented code from GitHubManager

--- Quickfeed/Managers/GitHubManager.swift
+++ Quickfeed/Managers/GitHubManager.swift
@@ -31,49 +31,11 @@ class LogInViewModel: NSObject, ObservableObject, ASWebAuthenticationPresentatio
         //guard let authURL = URL(string: "http://127.0.0.1:8081/app/login/login/github") else { return }
         let session = ASWebAuthenticationSession(url: authURL, callbackURLScheme: "quickfeed", completionHandler: { (callbackURL, error) in
             guard error == nil, let callbackURL = callbackURL else { return }
-            
-            let queryItems = URLComponents(string: callbackURL.absoluteString)?.queryItems
-            let tempCode = queryItems?.filter({ $0.name == "code" }).first?.value
 
-            
             print(callbackURL)
-            
-            //self.getAccessToken(authCode: tempCode!)
         })
         session.prefersEphemeralWebBrowserSession = false
         session.presentationContextProvider = self
         session.start()
     }
 }
-
-
-
-/*let url = URL(string: "http://www.thisismylink.com/postName.php")!
- var request = URLRequest(url: url)
- request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
- request.httpMethod = "POST"
- let parameters: [String: Any] = [
- "id": 13,
- "name": "Jack & Jill"
- ]
- request.httpBody = parameters.percentEncoded()
- 
- let task = URLSession.shared.dataTask(with: request) { data, response, error in
- guard let data = data,
- let response = response as? HTTPURLResponse,
- error == nil else {                                              // check for fundamental networking error
- print("error", error ?? "Unknown error")
- return
- }
- 
- guard (200 ... 299) ~= response.statusCode else {                    // check for http errors
- print("statusCode should be 2xx, but is \(response.statusCode)")
- print("response = \(response)")
- return
- }
- 
- let responseString = String(data: data, encoding: .utf8)
- print("responseString = \(responseString)")
- }
- 
- task.resume()*/
