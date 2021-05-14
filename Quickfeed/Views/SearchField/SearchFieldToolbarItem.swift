d83f7f99b56b5d7618686445f4e63b3d4d7842e2
Oskar Gjølga <os.gjolga@stud.uis.no>
2021-05-14 19:06:15 +0200

    rename searchfield

---
+++ Quickfeed/Views/SearchField/SearchFieldToolbarItem.swift
@@ -0,0 +1,20 @@
+//
+//  SearchFieldToolbarItem.swift
+//  Quickfeed
+//
+//  Created by Oskar Gjølga on 14/05/2021.
+//
+
+import SwiftUI
+
+struct SearchFieldToolbarItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchFieldToolbarItem_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchFieldToolbarItem()
+    }
+}
