50a2665308ecffa7c58e9fd3ceb7c7c6228a91a4
Bjørn Kristian Teisrud <b.k.teisrud@gmail.com>
2021-05-14 17:17:04 +0200

    Cleaner code LabPicker.swift

--- Quickfeed/Views/Pickers/LabPicker.swift
+++ Quickfeed/Views/Pickers/LabPicker.swift
@@ -2,14 +2,13 @@
 //  LabPicker.swift
 //  Quickfeed
 //
-//  Created by Oskar Gjølga on 09/02/2021.
-//
 
 import SwiftUI
 
 struct LabPicker: View {
     var labs: [Assignment]
     @Binding var selectedLab: UInt64
+    
     var body: some View {
         Picker(selection: $selectedLab, label: Text("Selected lab")) {
             ForEach(labs, id: \.id){ lab in
