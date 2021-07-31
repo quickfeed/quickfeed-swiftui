d53ab412ca3ae8aa661cdd749870957cab82c970
Bjørn Kristian Teisrud <b.k.teisrud@gmail.com>
2021-07-31 22:33:05 +0200

    appended different commands in QuickFeedApp.swift

--- Shared/QuickFeedApp.swift
+++ Shared/QuickFeedApp.swift
@@ -5,13 +5,34 @@
 //  Created by Bjørn Kristian Teisrud on 31/07/2021.
 //
 
+#if os(macOS)
+import SwiftUI
+import AppKit
+
+@main
+struct QuickfeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .commands{
+            SidebarCommands()
+            TextEditingCommands()
+            ToolbarCommands()
+        }
+    }
+}
+#endif
+
+#if os(iOS)
 import SwiftUI
 
 @main
-struct QuickFeedApp: App {
+struct QuickfeedApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+#endif
