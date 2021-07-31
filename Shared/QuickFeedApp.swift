d09f3b4f8d6429dfe370e748f2b1f93d6368d864
Bjørn Kristian Teisrud <b.k.teisrud@gmail.com>
2021-07-31 19:17:50 +0200

    Added targets for iOS, iPadOS and macOS

---
+++ Shared/QuickFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickFeedApp.swift
+//  Shared
+//
+//  Created by Bjørn Kristian Teisrud on 31/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct QuickFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
