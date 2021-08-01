092aa4b22fdf9e7a5e69b18fe17e8926b478d5e2
Bjørn Kristian Teisrud <b.k.teisrud@gmail.com>
2021-08-02 01:18:08 +0200

    Added screenShots of user views

--- iOS/Views/LogIn.swift
+++ iOS/Views/LogIn.swift
@@ -42,7 +42,7 @@ struct LogIn: View {
             .cornerRadius(10.0)
             .contentShape(Rectangle())
             .onTapGesture {
-                viewModel.setUser(userID: 27)
+                viewModel.setUser(userID: 100)
             }
         }
         .padding()
--- iOS/Views/User/UserProfile.swift
+++ iOS/Views/User/UserProfile.swift
@@ -13,6 +13,10 @@ struct UserProfile: View {
     @State var newInformation: Bool = false
     @State var newEnrollment: Bool = false
     
+    @State private var userName: String = ""
+    @State private var userEmail: String = ""
+    @State private var userStudentID: String = ""
+    
     var body: some View {
         VStack{
             HStack{
@@ -29,34 +33,62 @@ struct UserProfile: View {
                     .fontWeight(.bold)
                 Spacer()
                 Button(action: {
+                    viewModel.updateUser(name: userName, studentID: userStudentID, email: userEmail)
                     newInformation = !newInformation
                 }){
                     Text(newInformation ? "Done" : "Edit")
-                        .foregroundColor(.blue)
+                        .foregroundColor(newInformation && !(self.isValidEmail() && self.isValidStudentID() && self.isValidName()) ? .clear : .blue)
                 }
+                .disabled(newInformation && !(self.isValidEmail() && self.isValidStudentID() && self.isValidName()))
             }
             ScrollView{
                 VStack{
-                    HStack{
-                        Text("Name:")
-                        Spacer()
-                        Text(viewModel.user!.name)
+                    if newInformation {
+                        HStack{
+                            Text("Enter Name:")
+                            Spacer()
+                            TextField("Enter your name...", text: $userName)
+                                .foregroundColor(isValidName() ? .secondary : .red)
+                        }
+                        .padding(.top)
+                        Divider()
+                        HStack{
+                            Text("Enter Email:")
+                            Spacer()
+                            TextField("Enter your Email...", text: $userEmail)
+                                .foregroundColor(isValidEmail() ? .secondary : .red)
+                        }
+                        .padding(.top)
+                        Divider()
+                        HStack{
+                            Text("Enter StudentID:")
+                            Spacer()
+                            TextField("Enter your studentID...", text: $userStudentID)
+                                .foregroundColor(isValidStudentID() ? .secondary : .red)
+                        }
+                        .padding(.top)
+                    } else {
+                        HStack{
+                            Text("Name:")
+                            Spacer()
+                            Text(viewModel.user!.name)
+                        }
+                        .padding(.top)
+                        Divider()
+                        HStack{
+                            Text("Email:")
+                            Spacer()
+                            Text(viewModel.user!.email)
+                        }
+                        .padding(.top)
+                        Divider()
+                        HStack{
+                            Text("StudentID:")
+                            Spacer()
+                            Text(viewModel.user!.studentID)
+                        }
+                        .padding(.top)
                     }
-                    .padding(.top)
-                    Divider()
-                    HStack{
-                        Text("Email:")
-                        Spacer()
-                        Text(viewModel.user!.email)
-                    }
-                    .padding(.top)
-                    Divider()
-                    HStack{
-                        Text("StudentID:")
-                        Spacer()
-                        Text(viewModel.user!.studentID)
-                    }
-                    .padding(.top)
                 }
                 .padding()
                 Divider()
@@ -124,5 +156,25 @@ struct UserProfile: View {
             }
         }
         .padding()
+        .onAppear(perform: {
+            self.userName = viewModel.user!.name
+            self.userEmail = viewModel.user!.email
+            self.userStudentID = viewModel.user!.studentID
+        })
+    }
+    
+    func isValidName() -> Bool {
+        return self.userName.replacingOccurrences(of: " ", with: "").allSatisfy{ $0.isLetter }
+    }
+    
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: self.userEmail)
+    }
+    
+    func isValidStudentID() -> Bool {
+        return self.userStudentID.allSatisfy{ $0.isNumber } && !self.userStudentID.isEmpty
     }
 }
