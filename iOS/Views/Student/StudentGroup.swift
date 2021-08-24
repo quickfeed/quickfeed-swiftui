bc6ea70325659ad7354ef64aabb8fa7fa6e88f4d
Bjørn Kristian Teisrud <b.k.teisrud@gmail.com>
2021-08-24 16:37:54 +0200

    finished designing new group site for student. need functionality

--- iOS/Views/Student/StudentGroup.swift
+++ iOS/Views/Student/StudentGroup.swift
@@ -10,10 +10,45 @@ import SwiftUI
 struct StudentGroup: View {
     @ObservedObject var viewModel: StudentViewModel
     
+    @State private var groupName: String = ""
+    
     var body: some View {
-        Text("NewGroup")
-            .font(.title)
-            .fontWeight(.bold)
-        
+        VStack{
+            Text("NewGroup")
+                .font(.title)
+                .fontWeight(.bold)
+            HStack{
+                Text("GroupName:")
+                Spacer()
+                TextField("Enter your groupname...", text: $groupName)
+            }
+            Divider()
+            HStack{
+                Text("Members:")
+                Spacer()
+                Text(groupName)
+            }
+            Divider()
+            List{
+                Section(header: Text("Available students")){
+                    ForEach(viewModel.enrollments, id: \.self){ enrollment in
+                        if enrollment.user.id != viewModel.user.id{
+                            HStack{
+                                Text(enrollment.user.name)
+                                Spacer()
+                                Image(systemName: "plus")
+                                .buttonStyle(PlainButtonStyle())
+                            }
+                        }
+                    }
+                }
+            }
+            .cornerRadius(10.0)
+            Spacer()
+        }
+        .padding([.leading, .bottom, .trailing])
+        .onAppear(perform: {
+            viewModel.getEnrollmentsByCourse()
+        })
     }
 }
